28798ce4f3171d69008cb2bd37bb5ea60afc6f2f
Anton Dubrovin <44380520+AntonDubrovin@users.noreply.github.com>
2022-12-23 16:50:38 +0300

    Update bfs_seq.cpp

--- bfs_seq.cpp
+++ bfs_seq.cpp
@@ -6,6 +6,10 @@
 #include "Graph.h"
 #include <sys/time.h>
 
+#include <parlay/parallel.h>
+#include <parlay/primitives.h>
+#include <parlay/sequence.h>
+
 using namespace std;
 
 int cube_size = 500;
@@ -16,7 +20,7 @@ double wctime() {
     return tv.tv_sec + 1E-6 * tv.tv_usec;
 }
 
-vector<vector<vector<int>>> bfs(Node *start) {
+vector<vector<vector<int>>> bfs_seq(Node *start) {
     queue<Node> q;
     q.push(*start);
 
@@ -44,15 +48,79 @@ vector<vector<vector<int>>> bfs(Node *start) {
     return dist;
 }
 
+parlay::sequence<parlay::sequence<Node>> bfs_par(const Node *start) {
+    parlay::sequence<parlay::sequence<parlay::sequence<atomic_flag>>> visited =
+            parlay::tabulate<parlay::sequence<parlay::sequence<atomic_flag> >>(
+            cube_size + 1,
+            [&](const int x) {
+                return parlay::tabulate< parlay::sequence<atomic_flag>>(
+                        cube_size + 1,
+                        [&](const int y) {
+                            return parlay::tabulate<atomic_flag>(
+                                cube_size + 1,
+                                [&](const int z) {
+                                    return start->x == x && start->y == y && start->z == z;
+                                });
+                        });
+            });
+
+    parlay::sequence<Node> frontier(1, *start);
+    parlay::sequence<parlay::sequence<Node>> frontiers;
+
+    while (!frontier.empty()) {
+        frontiers.push_back(frontier);
+
+        const parlay::sequence<unsigned long long> deg = parlay::map(
+                frontier, [&](Node v) { return Graph::neighbours(v).size(); });
+
+        const auto deg_scan = parlay::scan(deg);
+
+        const parlay::sequence<unsigned long long> deg_scan_inclusive = deg_scan.first;
+
+        const unsigned long last = deg_scan.second;
+
+        parlay::sequence<Node> new_frontier =
+                parlay::tabulate(last, [&](const int i) { return Node(-1, -1, -1); });
+
+        parlay::parallel_for(0, frontier.size(), [&](size_t i) {
+            const Node node = frontier[i];
+            const parlay::sequence<Node> neighbours = Graph::neighbours(node);
+            parlay::parallel_for(0, neighbours.size(), [&](size_t u) {
+                const auto to = neighbours[u];
+
+                if (!visited[to.x][to.y][to.z].test_and_set(std::memory_order_relaxed)) {
+                    new_frontier[deg_scan_inclusive[i] + u] = to;
+                }
+            });
+        });
+
+        frontier = parlay::filter(new_frontier, [](Node node) {
+            return node.x != -1 && node.y != -1 && node.z != -1;
+        });
+    }
+
+    return frontiers;
+}
 
 int main() {
-    parlay::sequence < vector<vector<vector<int>>> seq_ress;
-    Node *start = new Node(0, 0, 0);
+    parlay::sequence<parlay::sequence<parlay::sequence<Node>>> parallel_res;
+    parlay::sequence<vector<vector<vector<int>>> sequential_res;
+
+    const Node *start = new Node(0, 0, 0);
+
+    parlay::internal::timer par_timer("par bfs");
     for (int i = 0; i < 5; i++) {
-        double start_time = wctime();
-        vector<vector<vector<int>>> seq_res = bfs(start);
-        double end_time = wctime();
-        cout << "Seq Time:" << end_time - start_time << endl;
-        seq_ress.push_back(seq_res);
+        parlay::sequence<parlay::sequence<Node>> par_res = bfs_par(start);
+        parallel_res.push_back(par_res);
+        par_timer.next("parallel");
     }
+    cout << "Parallel bfs " << par_timer.total_time() / 5 << endl;
+
+    parlay::internal::timer seq_timer("seq bfs");
+    for (int i = 0; i < 5; i++) {
+        vector<vector<Node>> seq_res = bfs_seq(start);
+        sequential_res.push_back(seq_res);
+        seq_timer.next("Sequential");
+    }
+    cout << "Sequential bfs " << seq_timer.total_time() / 5 << endl;
 }
